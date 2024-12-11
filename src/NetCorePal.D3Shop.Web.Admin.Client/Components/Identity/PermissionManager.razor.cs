d2d5b3538174bb8995645b6684377ed0c6ea4bb0
ZhengJie <zhengjie.work@outlook.com>
2024-12-11 15:37:36 +0800

    修复PermissionManager组件bug

--- src/NetCorePal.D3Shop.Web.Admin.Client/Components/Identity/PermissionManager.razor.cs
+++ src/NetCorePal.D3Shop.Web.Admin.Client/Components/Identity/PermissionManager.razor.cs
@@ -9,36 +9,81 @@ public sealed partial class PermissionManager : ComponentBase
     [Parameter] public EventCallback<List<string>> AssignedPermissionCodesChanged { get; set; }
     public string[] DisabledPermissionCodes { get; set; } = [];
 
-    private readonly Dictionary<string, Tree<Permission>> _treeDir = [];
-    private readonly Dictionary<string, IEnumerable<string>> _treeKeysDir = [];
-    private readonly Dictionary<string, Func<TreeEventArgs<Permission>, Task>> _treeOnChangeEventDir = [];
+    private readonly Dictionary<string, Tree<Permission>> _treeDir = new();
+    private readonly Dictionary<string, IEnumerable<string>> _treeKeysDir = new();
     private PermissionGroup[] PermissionGroups { get; set; } = [];
-    private Guid Id { get; set; }
+    private Guid TreeComponentKey { get; set; }
 
     protected override void OnInitialized()
     {
         PermissionGroups = PermissionDefinitionContext.PermissionGroups.ToArray();
         foreach (var group in PermissionGroups)
         {
-            var treeId = group.Name;
-            _treeDir.Add(treeId, default!);
-            _treeKeysDir.Add(treeId, group.PermissionsWithChildren.Select(p => p.Code));
-            _treeOnChangeEventDir.Add(treeId, async args =>
-            {
-                foreach (var key in _treeKeysDir[args.Tree.Id])
-                {
-                    AssignedPermissionCodes.Remove(key);
-                }
-
-                AssignedPermissionCodes.AddRange(args.Tree.CheckedKeys);
-
-                await AssignedPermissionCodesChanged.InvokeAsync(AssignedPermissionCodes);
-            });
+            var groupName = group.Name;
+            _treeDir[groupName] = default!;
+            _treeKeysDir[groupName] = group.PermissionsWithChildren.Select(p => p.Code);
         }
     }
 
-    public void Update()
+    private async Task OnTreeChange(TreeEventArgs<Permission> args, string groupName)
     {
-        Id = Guid.NewGuid();
+        foreach (var key in _treeKeysDir[groupName])
+        {
+            AssignedPermissionCodes.Remove(key);
+        }
+        AssignedPermissionCodes.AddRange(args.Tree.CheckedKeys);
+
+        await AssignedPermissionCodesChanged.InvokeAsync(AssignedPermissionCodes);
+    }
+
+    private async Task HandleCheckAll(bool isChecked, string? groupName = null)
+    {
+        if (groupName == null)
+        {
+            foreach (var tree in _treeDir.Values)
+            {
+                if (isChecked)
+                    tree.CheckAll();
+                else
+                    tree.UncheckAll();
+            }
+
+            AssignedPermissionCodes.Clear();
+            if (isChecked)
+            {
+                AssignedPermissionCodes.AddRange(_treeKeysDir.Values.SelectMany(k => k));
+            }
+        }
+        else
+        {
+            if (_treeDir.TryGetValue(groupName, out var tree))
+            {
+                if (isChecked)
+                    tree.CheckAll();
+                else
+                    tree.UncheckAll();
+            }
+
+            AssignedPermissionCodes.RemoveAll(code => _treeKeysDir[groupName].Contains(code));
+            if (isChecked)
+            {
+                AssignedPermissionCodes.AddRange(_treeKeysDir[groupName]);
+            }
+        }
+
+        await AssignedPermissionCodesChanged.InvokeAsync(AssignedPermissionCodes);
+    }
+
+    private Task OnCheckAll() => HandleCheckAll(true);
+
+    private Task OnUncheckAll() => HandleCheckAll(false);
+
+    private Task OnCheckGroupAll(string groupName) => HandleCheckAll(true, groupName);
+
+    private Task OnUncheckGroupAll(string groupName) => HandleCheckAll(false, groupName);
+
+    public void ReRenderTree()
+    {
+        TreeComponentKey = Guid.NewGuid();
     }
 }
