1459a64e114f0c90cd1cc708372ed616de38edf7
ZhengJie <zhengjie.work@outlook.com>
2024-12-11 08:33:51 +0800

    调整Dto文件位置

--- src/NetCorePal.D3Shop.Web/Blazor/Components/Account/Login.razor.cs
+++ src/NetCorePal.D3Shop.Web/Blazor/Components/Account/Login.razor.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Options;
 using NetCorePal.D3Shop.Admin.Shared.Requests;
 using NetCorePal.D3Shop.Web.Application.Queries.Identity;
-using NetCorePal.D3Shop.Web.Application.Queries.Identity.QueryResult;
+using NetCorePal.D3Shop.Web.Controllers.Identity.Dto;
 using NetCorePal.D3Shop.Web.Helper;
 
 namespace NetCorePal.D3Shop.Web.Blazor.Components.Account;
