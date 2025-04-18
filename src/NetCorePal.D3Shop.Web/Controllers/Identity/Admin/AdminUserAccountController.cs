a46b478ae70a8ee083afbad0a0eb0e76590067df
zhouweitao <123456>
2025-04-18 09:22:08 +0800

    合并登录

--- src/NetCorePal.D3Shop.Web/Controllers/Identity/Admin/AdminUserAccountController.cs
+++ src/NetCorePal.D3Shop.Web/Controllers/Identity/Admin/AdminUserAccountController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using NetCorePal.D3Shop.Admin.Shared.Requests;
+using NetCorePal.D3Shop.Admin.Shared.Responses;
+using NetCorePal.D3Shop.Domain.AggregatesModel.Identity.RoleAggregate;
 using NetCorePal.D3Shop.Web.Application.Queries.Identity.Admin;
 using NetCorePal.D3Shop.Web.Controllers.Identity.Admin.Dto;
 using NetCorePal.D3Shop.Web.Helper;
@@ -18,6 +20,7 @@ namespace NetCorePal.D3Shop.Web.Controllers.Identity.Admin;
 [AllowAnonymous]
 public class AdminUserAccountController(
     AdminUserQuery adminUserQuery,
+    TokenGenerator tokenGenerator,
     IOptions<AppConfiguration> appConfiguration) : ControllerBase
 {
     private AppConfiguration AppConfiguration => appConfiguration.Value;
@@ -59,4 +62,31 @@ public class AdminUserAccountController(
 
         return claims;
     }
+
+    [HttpPost("auth/login")]
+    public async Task<ResponseData<AdminUserLoginResponse>> Login([FromBody] AdminUserLoginRequest request)
+    {
+        //重定向地址
+        var redirectUri = HttpContext.Request?.Query["redirect"].FirstOrDefault();
+        if (string.IsNullOrEmpty(redirectUri))
+        {
+            redirectUri = "/analytics";
+        }
+        var authInfo =
+            await adminUserQuery.GetUserInfoForLoginAsync(request.Name, HttpContext.RequestAborted);
+        if (authInfo is null)
+            throw new KnownException("无效的用户", -1);
+
+        if (!PasswordHasher.VerifyHashedPassword(request.Password, authInfo.Password))
+            throw new KnownException("密码错误", -1);
+
+        var refreshToken = TokenGenerator.GenerateRefreshToken();
+        ICollection<RoleId> roles = await adminUserQuery.GetAssignedRoleIdsAsync(authInfo.Id, HttpContext.RequestAborted);
+        var token = await tokenGenerator.GenerateJwtAsync([
+            new Claim(ClaimTypes.NameIdentifier, authInfo.Id.ToString()),
+                new Claim(ClaimTypes.Name, authInfo.Name)
+        ]);
+        return AdminUserLoginResponse.Success(token, refreshToken, authInfo.Id, authInfo.Name, authInfo.Name, roles, redirectUri).AsResponseData();
+    }
+
 }
--- src/NetCorePal.D3Shop.Web/Controllers/Identity/VueAdmin/Requests/VueUserLoginRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace NetCorePal.D3Shop.Web.Controllers.Identity.VueAdmin.Requests
-{
-    public record VueUserLoginRequest(string UserName,string Password);
-       
-}
